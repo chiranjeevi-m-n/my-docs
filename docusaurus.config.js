e698c57284b7b31e70f5f694182bb051f7abfc6e
Chiranjeevi <chiranjeevi.mn@cirruslabs.io>
2025-02-22 00:39:35 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://Deloitte_gloal_cloud_services.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/my-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
